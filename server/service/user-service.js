0806b677b3473c22caded3c5b982550fc074d2fb
Laptev Sergei <sl260401@gmail.com>
2023-07-18 23:27:41 +0300

    logout

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -62,6 +62,11 @@ class UserServie {
 			user: userDto
 		}
 	}
+
+	async logout(refreshToken) {
+		const token = await tokenService.removeToken(refreshToken)
+		return token
+	}
 }
 
 module.exports = new UserServie()
